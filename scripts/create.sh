b5587b5a5631325cf67706bc84332bd47f112766
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-05-11 14:34:44 +0200

    Set Cloud Build

--- scripts/create.sh
+++ scripts/create.sh
@@ -37,6 +37,8 @@ enable_project_api "${PROJECT_ID}" cloudfunctions.googleapis.com
 enable_project_api "${PROJECT_ID}" storage-api.googleapis.com
 enable_project_api "${PROJECT_ID}" storage-component.googleapis.com
 enable_project_api "${PROJECT_ID}" cloudbuild.googleapis.com
+enable_project_api "${PROJECT_ID}" secretmanager.googleapis.com
+enable_project_api "${PROJECT_ID}" cloudkms.googleapis.com
 
 
 get_terraform_backend_bucket
