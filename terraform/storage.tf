b226d6388dc9310439fa82b6e0fee976ca68e847
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-05-12 11:05:39 +0200

    Remove versioning for functions source bucket
    Since hash is added to every file (as workaround) versioning is not necessary

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -2,8 +2,4 @@ resource "google_storage_bucket" "functions" {
   name          = "${var.project_id}-functions"
   location      = var.region
   force_destroy = true
-
-  versioning {
-    enabled = "true"
-  }
 }
